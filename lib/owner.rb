ef406664be25c15ca506ea68ba6e03ecc1bce4f7
Whittney Davis <wcdavis89@yahoo.com>
2019-12-02 00:25:31 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,91 +1,72 @@
 class Owner
-  @@all= []
   attr_accessor :name, :pets
   attr_reader :species
 
-  # Instance Methods #
+  @@owners = []
 
-  def initialize(species)
+  def initialize(species, pets = {:fishes => [], :dogs => [], :cats => []})
     @species = species
-    @@all << self
-    @pets = {:fishes => [], :dogs => [], :cats => []}
+    @pets = pets
+    @@owners << self
   end
 
   def say_species
-    return "I am a #{@species}."
+    "I am a #{@species}."
   end
 
-  # Pets #
-
-  def buy_fish(name_of_fish)
-    @pets[:fishes] << Fish.new(name_of_fish)
+  def buy_fish(fish_name)
+    @pets[:fishes] << Fish.new(fish_name)
   end
 
-  def buy_dog(name_of_dog)
-    @pets[:dogs] << Dog.new(name_of_dog)
+  def buy_cat(cat_name)
+    @pets[:cats] << Cat.new(cat_name)
   end
 
-  def buy_cat(name_of_cat)
-    @pets[:cats] << Cat.new(name_of_cat)
+  def buy_dog(dog_name)
+    @pets[:dogs] << Dog.new(dog_name)
   end
 
   def walk_dogs
-    @pets.collect do |species, instances|
-      if species == :dogs
-        instances.each do |dog|
-          dog.mood = "happy"
-        end
-      end
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
     end
   end
 
   def play_with_cats
-    @pets.collect do |species, instances|
-      if species == :cats
-        instances.each do |cat|
-          cat.mood = "happy"
-        end
-      end
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
     end
   end
 
   def feed_fish
-    @pets.collect do |species, instances|
-      if species == :fishes
-        instances.each do |fish|
-          fish.mood = "happy"
-        end
-      end
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
     end
   end
 
   def sell_pets
-    @pets.collect do |species, instances|
-      instances.each do |pet|
+    @pets.each do |type, name_array|
+      name_array.each do |pet|
         pet.mood = "nervous"
-      end
-      instances.clear
+        #name_array.delete(pet)
+      end 
     end
+    @pets = {}
   end
 
   def list_pets
-    num_dogs = @pets[:dogs].size
-    num_cats = @pets[:cats].size
-    num_fish = @pets[:fishes].size
-    return "I have #{num_fish} fish, #{num_dogs} dog(s), and #{num_cats} cat(s)."
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
   end
 
-  # Class Methods #
-
   def self.all
-    @@all
+    @@owners
   end
 
   def self.reset_all
-    @@all.clear
+    @@owners.clear
   end
 
-  def self.count
-    @@all.size
+  def self. count
+    @@owners.length
   end
 end
