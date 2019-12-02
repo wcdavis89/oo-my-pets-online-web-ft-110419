b5ff101698934e91f5fb74a4ff0e76df327f397a
Whittney Davis <wcdavis89@yahoo.com>
2019-12-02 00:29:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,18 @@
 class Dog
-  attr_accessor :mood
+  # code goes here
+  attr_accessor :mood, :owner
   attr_reader :name
 
-  def initialize(name)
+  @@all = []
+
+  def initialize(name, owner)
     @name = name
     @mood = "nervous"
+    @owner = owner
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 end
